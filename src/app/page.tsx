bb97a4f98a992480ceeb70d31ead685409214f22
Noc-art <nevinthang02@gmail.com>
2023-11-06 16:06:51 +0700

    bikin navbar 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
                         <li>
                           <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign Out</a>
                         </li>
+                      </ul>
                   </div>
               </li>
               <li>
